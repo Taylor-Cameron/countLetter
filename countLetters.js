5be5d1915c90379d31a2b119c5e0720d72672b21
Taylor-Cameron <taylorcameron95@hotmail.ca>
2018-10-24 19:21:42 +0000

    count letter assigment

--- countLetters.js
+++ countLetters.js
@@ -1,11 +1,4 @@
-// 1. function count Letters **
-// 2. create an empty object **
-// 3. remove spaces and to lower case **
-// 4. iterate through string input **
-// 5. identify all different letters
-// 6. count the amount of times a letter is present
-// 7. identify the keys of the object as letters and the amount of times
-// it is present as value
+
 function countLetters(string) {
   var finalObject = {};
   var noSpaces = string.replace(/\ /g, '').toLowerCase();
